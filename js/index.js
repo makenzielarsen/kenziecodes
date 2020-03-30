0a515eecbe23abce831e2706966f8d0e37e655ed
makenzielarsen <makenzie97larsen@gmail.com>
2020-03-30 16:02:15 -0600

    Working on the landing page

--- js/index.js
+++ js/index.js
@@ -1,36 +1,19 @@
-let canvas = null;
-let context = null;
-let previousTime = performance.now();
-
-function initialize() {
-  canvas = document.getElementById("canvas");
-  context = canvas.getContext("2d");
-
-  requestAnimationFrame(gameLoop);
+function meToFrom() {
+  document.getElementById("aboutMeDiv").style.display = "none";
+  document.getElementById("fromDiv").style.display = "flex";
 }
 
-function gameLoop(time) {
-  let elapsedTime = time - previousTime;
-  previousTime = time;
-  update(elapsedTime);
-
-  render();
-
-  window.requestAnimationFrame(gameLoop);
+function fromToUSU() {
+  document.getElementById("fromDiv").style.display = "none";
+  document.getElementById("usuDiv").style.display = "flex";
 }
 
-function update(elapsedTime) {}
+function USUToMajors() {
+  document.getElementById("usuDiv").style.display = "none";
+  document.getElementById("majorsDiv").style.display = "flex";
+}
 
-function render() {
-  // context.drawImage(
-  //   ship,
-  //   2 * subImageWidth,
-  //   0,
-  //   subImageWidth,
-  //   ship.height,
-  //   startX,
-  //   startY,
-  //   sizeX,
-  //   sizeY
-  // );
+function majorsToMe() {
+  document.getElementById("majorsDiv").style.display = "none";
+  document.getElementById("aboutMeDiv").style.display = "flex";
 }
